76ea5d28c0558dbc92bfa0b72e0150eb46f1d225
nicholascok <nicholasoc@outlook.com>
2021-09-11 21:07:50 -0230

    Update ps2_device.ino

--- ps2_device.ino
+++ ps2_device.ino
@@ -6,6 +6,100 @@
 #include <Arduino.h>
 #include "ps2_device.h"
 
+/* returns the ascii char represnting a key */
+char* get_char(int _k) {
+  switch (_k) {
+    case KEY_A:
+      return "A";
+      break;
+    case KEY_B:
+      return "B";
+      break;
+    case KEY_C:
+      return "C";
+      break;
+    case KEY_D:
+      return "D";
+      break;
+    case KEY_E:
+      return "E";
+      break;
+    case KEY_F:
+      return "F";
+      break;
+    case KEY_G:
+      return "G";
+      break;
+    case KEY_H:
+      return "H";
+      break;
+    case KEY_I:
+      return "I";
+      break;
+    case KEY_J:
+      return "J";
+      break;
+    case KEY_K:
+      return "K";
+      break;
+    case KEY_L:
+      return "L";
+      break;
+    case KEY_M:
+      return "M";
+      break;
+    case KEY_N:
+      return "N";
+      break;
+    case KEY_O:
+      return "O";
+      break;
+    case KEY_P:
+      return "P";
+      break;
+    case KEY_Q:
+      return "Q";
+      break;
+    case KEY_R:
+      return "R";
+      break;
+    case KEY_S:
+      return "S";
+      break;
+    case KEY_T:
+      return "T";
+      break;
+    case KEY_U:
+      return "U";
+      break;
+    case KEY_V:
+      return "V";
+      break;
+    case KEY_W:
+      return "W";
+      break;
+    case KEY_X:
+      return "X";
+      break;
+    case KEY_Y:
+      return "Y";
+      break;
+    case KEY_Z:
+      return "Z";
+      break;
+    case KEY_SPACE:
+      return " ";
+    case KEY_DOT:
+      return ".";
+      break;
+    case KEY_COMMA:
+      return ",";
+    default:
+      return "U";
+      break;
+  }
+}
+
 void enp(int _p) {
   pinMode(_p, INPUT_PULLUP);
   return;
